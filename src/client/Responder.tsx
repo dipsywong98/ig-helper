59b66ee0bcd7eeeca054c0009bfac50fc7c644a8
Dipsy Wong <ycwongal@connect.ust.hk>
2023-10-28 15:40:05 +0800

    adjust image scaling

--- src/client/Responder.tsx
+++ src/client/Responder.tsx
@@ -7,9 +7,12 @@ interface Props {
   question: string
   response: string
   domScale?: number
+  imageScale?: number
 }
 
-export function Responder({ response, question, domScale: scale }: Props) {
+export function Responder({
+  response, question, domScale: scale, imageScale,
+}: Props) {
   const domRef = useRef(null);
   const [dataUrl, setDataUrl] = useState('');
   useEffect(() => {
@@ -19,47 +22,56 @@ export function Responder({ response, question, domScale: scale }: Props) {
       });
     }
   }, [scale]);
+  const domBubble = (ref?: typeof domRef) => (
+    <Box
+      ref={ref}
+      sx={{
+        borderRadius: '30px',
+        width: '900px',
+        overflow: 'hidden',
+        fontFamily: '"Roboto", "Noto Sans", "Helvetica", "Arial", sans-serif',
+        textAlign: 'center',
+      }}
+    >
+      <Box
+        sx={{
+          padding: '50px 78px',
+          backgroundColor: '#262626',
+          fontSize: '48px',
+          color: '#ffffff',
+        }}
+      >
+        {question}
+      </Box>
+      <Box sx={{
+        backgroundColor: '#ffffff',
+        color: '#292929',
+        padding: '80px 70px',
+        fontSize: '64px',
+      }}
+      >
+        {response}
+      </Box>
+    </Box>
+  );
   return (
     <Box sx={{ margin: 1, width: '225px', height: '119px' }}>
       <Box
-        style={dataUrl !== '' ? { width: 0, height: 0, overflow: 'hidden' } : { }}
+        sx={{ width: 0, height: 0, overflow: 'hidden' }}
       >
-        <Box
-          ref={domRef}
-          sx={{
-            borderRadius: `${15 * scale}px`,
-            width: `${450 * scale}px`,
-            overflow: 'hidden',
-            fontFamily: '"Roboto", "Noto Sans", "Helvetica", "Arial", sans-serif',
-            textAlign: 'center',
-          }}
-        >
-          <Box
-            sx={{
-              padding: `${25 * scale}px ${39 * scale}px`,
-              backgroundColor: '#262626',
-              fontSize: `${24 * scale}px`,
-              color: '#ffffff',
-            }}
-          >
-            {question}
-          </Box>
-          <Box sx={{
-            backgroundColor: '#ffffff',
-            color: '#292929',
-            padding: `${40 * scale}px ${35 * scale}px`,
-            fontSize: `${32 * scale}px`,
-          }}
-          >
-            {response}
-          </Box>
-        </Box>
+        {domBubble(domRef)}
       </Box>
-      {dataUrl && <img alt="response" src={dataUrl} className="respond-img" />}
+      <Box sx={{ scale: `${imageScale}`, transformOrigin: 'top left' }}>
+        {dataUrl
+          ? (
+            <img alt="response" src={dataUrl} />
+          ) : domBubble()}
+      </Box>
     </Box>
   );
 }
 
 Responder.defaultProps = {
-  domScale: 0.5,
+  domScale: 1,
+  imageScale: 0.25,
 };
